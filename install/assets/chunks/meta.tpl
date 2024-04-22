e837c2ea4dcf240c1d1c5e0d7aff458c6e390125
JuliaCherri <34334110+JuliaCherri@users.noreply.github.com>
2024-04-22 15:50:38 +0400

    fix

--- install/assets/chunks/meta.tpl
+++ install/assets/chunks/meta.tpl
@@ -7,10 +7,17 @@
  */
 
 <title>[*meta_title*] - [(site_name)]</title>
+<meta property="og:title" content="[*meta_title*] - [(site_name)]">
 <meta name="description" content="[*meta_description*]">
+<meta property="og:description" content="[*meta_description*]">
 [*meta_noindex*]
 <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
 <link rel="icon" href="favicon.png" type="image/png">
-<link rel="image_src" href="[*image-src*]">
+<link rel="image_src" href="[*image_src*]">
+<meta property="og:image" content="[*image_src*]">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="[(site_name)]">
+
 <meta http-equiv="Content-Type" content="text/html; charset=[(modx_charset)]">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <base href="[(site_url)]">
--- views/parts/meta.blade.php
+++ views/parts/meta.blade.php
@@ -1,11 +1,23 @@
 <title>{!! !empty($meta_title) ? $meta_title : $pagetitle !!} - {!! evo()->getConfig('site_name') !!}</title>
+<meta property="og:title" content="{!! !empty($meta_title) ? $meta_title : $pagetitle !!} - {!! evo()->getConfig('site_name') !!}">
 <meta name="description" content="{!! !empty($meta_description) ? $meta_description : $introtext !!}">
+<meta property="og:description" content="{!! !empty($meta_description) ? $meta_description : $introtext !!}">
 {!! $meta_noindex !!}
 <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
 <link rel="icon" href="favicon.png" type="image/png">
-@if ( !empty($image_src) ) <link rel="image_src" href="{!! $image_src !!}">
-@elseif ( !empty($image) ) <link rel="image_src" href="{!! $image !!}">
-@else <link rel="image_src" href="img/socials.png">
+@if ( !empty($image_src) ) 
+    <link rel="image_src" href="{!! $image_src !!}">
+    <meta property="og:image" content="{!! $image_src !!}">
+@elseif ( !empty($image) ) 
+    <link rel="image_src" href="{!! $image !!}">
+    <meta property="og:image" content="{!! $image !!}">
+@else 
+    <link rel="image_src" href="img/socials.png">
+    <meta property="og:image" content="img/socials.png">
 @endif
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="{!! evo()->getConfig('site_name') !!}">
+
 <meta http-equiv="Content-Type" content="text/html; charset={!! evo()->getConfig('modx_charset') !!}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <base href="{!! evo()->getConfig('site_url') !!}">
